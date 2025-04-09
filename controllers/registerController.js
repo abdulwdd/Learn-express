b3736fa4a06b0d0ebb57c89758539cb1fad0ea17
Abdulwaheed Abdullahi Omotayo <abdullahiowd@gmail.com>
2025-04-09 14:50:13 +0100

    doyin

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,14 +1,17 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 const ROLES_LIST = require("../config/roles_list")
 
 const handleNewUser = async (req, resp) => {
     const { user, pword, role } = req.body;
     console.log(`role gotten ${role}`);
+    console.log(`${JSON.stringify(ROLES_LIST)}`);
     if (!user || !pword) return resp.status(400).json({ "Message": "Username and password was not received" }); //this cheks if the request came in with a username and password
 
     if (!ROLES_LIST.hasOwnProperty(role)) {
         return resp.status(400).json({ "Message": `Invalid role provided: ${role}. Allowed roles are: ${ROLES_LIST}` });
     } else {
-        role = { [role]: ROLES_LIST[role] };
+        roles = { [role]: ROLES_LIST[role] };
 
     }
 
@@ -24,7 +27,15 @@ const handleNewUser = async (req, resp) => {
             "username": user,
             "password": hashedPword,
             "realPassword": pword,
-            "roles": role
+            "roles": roles
         })
-      }
+
+        console.log(result);
+        resp.status(201).json({ "message": `The person ${user} has been created` });
+    } catch (err) {
+        return resp.status(500).json({ "message": err.message })
     }
+
+}
+
+module.exports = { handleNewUser }
